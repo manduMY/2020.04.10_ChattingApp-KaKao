39541c30d698f96f6db2be8bbeb56deea48d8238
manduMY <poi31320@naver.com>
2020-04-21 20:22:38 +0900

    merge feature/BottomNavigation_Bar And develop

--- app/src/main/java/com/example/chattingapp/FriendListActivity.java
+++ app/src/main/java/com/example/chattingapp/FriendListActivity.java
@@ -1,27 +1,32 @@
 package com.example.chattingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+
 public class FriendListActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
 
-        ImageButton btnMy = (ImageButton) findViewById(R.id.MyBtn);
-        btnMy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in = new Intent(FriendListActivity.this, MyActivity.class);
-                startActivity(in);
-            }
-        });
+
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        BottomNavigationHelper.removeShiftMode(bottomNavigationView);
+
     }
+
 }
