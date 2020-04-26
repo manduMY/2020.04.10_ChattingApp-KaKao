2a75fb24dd25ad92cbd8ace0bfa46ca3dede00b0
manduMY <poi31320@naver.com>
2020-04-26 22:00:05 +0900

    firebase SignUp And SignIn

--- app/src/main/java/com/example/chattingapp/MainActivity.java
+++ app/src/main/java/com/example/chattingapp/MainActivity.java
@@ -1,157 +1,61 @@
 package com.example.chattingapp;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.MenuItem;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SignInButton googleSignIn;
-    private GoogleSignInClient mGoogleSignInClient;
-    private static final String TAG = "MainActivity";
-    private FirebaseAuth mAuth;
-    private Button btnSignOut;
-    private int RC_SIGN_IN = 1;
+    BottomNavigationView bottomNavigationView;
 
-    Button btnLogin;
-    Button btnRegister;
+    FriendListFragment menuFriendListFragment;
+    ChatListFragment menuChatListFragment;
+    MoreListFragment menuMoreListFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        googleSignIn = findViewById(R.id.Btn_Google_SignIn);
-        mAuth = FirebaseAuth.getInstance();
-        btnSignOut = findViewById(R.id.btn_SignOut);
+        // Fragment 생성
+        menuFriendListFragment = new FriendListFragment();
+        menuChatListFragment = new ChatListFragment();
+        menuMoreListFragment = new MoreListFragment();
 
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
+        //제일 처음 띄워줄 뷰를 세팅해줍니다. commit();까지 해줘야 합니다.
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,menuFriendListFragment).commitAllowingStateLoss();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        //bottomnavigationview의 아이콘을 선택 했을때 원하는 프래그먼트가 띄워질 수 있도록 리스너를 추가합니다.
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        googleSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn();
-            }
-        });
+        menuFriendListFragment = new FriendListFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, menuFriendListFragment).commit();
 
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuth.signOut();
-                Toast.makeText(MainActivity.this, "Signed Out !!", Toast.LENGTH_SHORT).show();
-                btnSignOut.setVisibility(View.INVISIBLE);
-            }
-        });
 
-        btnLogin = (Button)findViewById(R.id.BtnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                Intent in = new Intent(MainActivity.this,FriendListActivity.class);
-                startActivity(in);
-            }
-        });
-        btnRegister = (Button)findViewById(R.id.BtnRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "회원가입 창 이동", Toast.LENGTH_SHORT).show();
-                Intent in = new Intent(MainActivity.this,RegisterActivity.class);
-                startActivity(in);
-            }
-        });
     }
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_friendList:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,menuFriendListFragment).commitAllowingStateLoss();
+                    return true;
+                case R.id.navigation_chatList:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,menuChatListFragment).commitAllowingStateLoss();
+                    return true;
+                case R.id.navigation_moreList:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,menuMoreListFragment).commitAllowingStateLoss();
+                    return true;
+                default: return false;
+            }
         }
-    }
+    };
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try{
-            GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
-            Toast.makeText(MainActivity.this, "Signed In Successfully", Toast.LENGTH_SHORT).show();
-            firebaseAuthWithGoogle(acc);
-        }catch(ApiException e) {
-            Toast.makeText(MainActivity.this, "Signed In Failed", Toast.LENGTH_SHORT).show();
-            firebaseAuthWithGoogle(null);
-        }
-    }
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
-    }
-
-    private void updateUI(FirebaseUser fUser) {
-        btnSignOut.setVisibility(View.VISIBLE);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-        if (account != null) {
-            String personName = account.getDisplayName();
-            String personGivenName = account.getGivenName();
-            String personFamilyName = account.getFamilyName();
-            String personEmail = account.getEmail();
-            String personId = account.getId();
-            Uri personPhoto = account.getPhotoUrl();
-
-            Toast.makeText(MainActivity.this, personName + personEmail, Toast.LENGTH_SHORT).show();
-        }
-    }
 }
--- app/src/main/java/com/example/chattingapp/SignUpActivity.java
+++ app/src/main/java/com/example/chattingapp/SignUpActivity.java
@@ -1,8 +1,5 @@
 package com.example.chattingapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,20 +7,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class RegisterActivity extends AppCompatActivity {
-    private static final String TAG = "RegisterActivity";
-    private FirebaseAuth firebaseAuth;
+public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
 
     private EditText editTextEmail;
     private EditText editTextPassword;
     private EditText editTextName;
-    private Button btnSubmit;
+    private Button btnSignUp;
 
     private FirebaseAuth mAuth;
 
@@ -38,62 +38,59 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
-        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_sign_up);
 
         // Initialize Firebase Auth
-        firebaseAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
-        editTextEmail = (EditText)findViewById(R.id.editText_email);
-        editTextPassword = (EditText)findViewById(R.id.editText_password);
-        editTextName = (EditText)findViewById(R.id.editText_name);
+        editTextEmail = (EditText)findViewById(R.id.SignUp_editText_email);
+        editTextPassword = (EditText)findViewById(R.id.SignUp_editText_password);
+        editTextName = (EditText)findViewById(R.id.SignUp_editText_name);
 
-        btnSubmit = (Button)findViewById(R.id.BtnSubmit);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        btnSignUp = (Button)findViewById(R.id.SignUp_BtnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String strEmail = editTextEmail.getText().toString();
-                String strPassword = editTextPassword.getText().toString();
+                final String strPassword = editTextPassword.getText().toString();
                 String strName = editTextName.getText().toString();
                 if(strEmail.isEmpty()) {
-                    Toast.makeText(RegisterActivity.this,"Email을 입력해 주십시오.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this,"Sign Up: Email을 입력해 주십시오.",Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if(strPassword.isEmpty()) {
-                    Toast.makeText(RegisterActivity.this,"Password를 입력해 주십시오.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this,"Sign Up: Password를 입력해 주십시오.",Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if(strName.isEmpty()) {
-                    Toast.makeText(RegisterActivity.this,"Name을 입력해 주십시오.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this,"Sign Up: Name을 입력해 주십시오.",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(strPassword.length() < 8) {
+                    Toast.makeText(SignUpActivity.this,"Sign Up: 비밀번호를 8자 이상 입력해 주십시오.",Toast.LENGTH_SHORT).show();
                     return;
                 }
                 mAuth.createUserWithEmailAndPassword(strEmail, strPassword)
-                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    finish();
+//                                    FirebaseUser user = mAuth.getCurrentUser();
 //                                    updateUI(user);
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
 //                                    updateUI(null);
                                 }
 
                                 // ...
                             }
                         });
-
-
             }
         });
     }
-
-
-
 }
