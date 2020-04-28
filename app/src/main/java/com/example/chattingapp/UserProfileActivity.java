2e895b729c3138f57181966bffa9ed3158190dc2
manduMY <poi31320@naver.com>
2020-04-28 23:43:01 +0900

    implements picture's upload

--- app/src/main/java/com/example/chattingapp/UserProfileActivity.java
+++ app/src/main/java/com/example/chattingapp/UserProfileActivity.java
@@ -1,18 +1,57 @@
 package com.example.chattingapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UserProfileActivity extends AppCompatActivity {
+import java.io.IOException;
 
-    private ImageButton user_profile;
+public class UserProfileActivity extends AppCompatActivity {
+    private static int PICK_IMAGE = 123;
+    private ImageButton profileImageView;
+    Uri imagePath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        profileImageView = (ImageButton) findViewById(R.id.updateImage);
+        profileImageView.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent profileIntent = new Intent();
+                profileIntent.setType("image/*");
+                profileIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(profileIntent, "Select Image."), PICK_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null) {
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagePath);
+                profileImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
+
 }
